b27075349064cb554dd00387d92d61bd93394825
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 13:24:08 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -2,7 +2,7 @@ public class Ejercicio3{
     public static void main (String [] args){
         // Declaro todo lo que me pide el ejercicio
         String name = "Pablo García Muñoz";
-        String direction = "Avenida España nº17";
+        String direction = "Avenida España nº1";
         String phone = "999999999";
         // Lo muestro por pantalla
         System.out.println(name);
