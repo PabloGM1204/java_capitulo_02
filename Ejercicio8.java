ce318c8711e9010f16ca938fe2457d9c0bbeabf8
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 20:34:09 +0200

    Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,13 @@
+public class Ejercicio8{
+    public static void main (String [] args){
+        // Creacion de los 5 char
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+        char d = 'd';
+        char e = 'e';
+        // Creo la cadena de caracteres y le asigno el valor de los cinco char
+        String cadena = "" + a + b + c + d + e;
+        System.out.println(cadena);
+    }
+}
