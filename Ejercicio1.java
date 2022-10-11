6c8bb82c606ac5b043a6e656984a29eeaa84c76e
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 13:09:14 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,5 +1,12 @@
 public class Ejercicio1{
     public static void main(String [] args) {
-        
+        // Creo y declaro los valores
+        int x = 144;
+        int y = 999;
+        System.out.println("x = "+x+" y = "+y); // Escribe los valores de cada uno
+        System.out.println("x + y = "+(x+y)); // Suma de los valores
+        System.out.println("x - y = "+(x-y)); // Resta de los valores
+        System.out.println("x * y = "+(x*y)); // Multiplicación de los valores 
+        System.out.println("x / y = "+(x/y)); // Division de los valores
     }
 }
