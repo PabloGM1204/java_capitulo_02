992e173fa912301d270d75cc450290b78c2d54d6
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 13:13:45 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2{
+    public static void main(String [] args){ 
+        // Creo un string que permite cadena de texto y la pongo en pantalla
+        String name = "Pablo García Muñoz";
+        System.out.println(name);
+    }
+}
