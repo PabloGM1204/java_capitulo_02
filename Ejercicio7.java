262b85959baa3093b27813a7d69be69c62abde29
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 20:25:05 +0200

    Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,11 @@
+public class Ejercicio7{
+    public static void main (String [] args){
+        // Declaro las variables Strings
+        String primero = "Primera";
+        String segunda = "Segundo";
+        // Declaro las varibales char
+        char letra1 = 'a';
+        char letra2 = 'b';
+        System.out.println(letra1 +""+ letra2 + primero + segunda );
+    }
+}
