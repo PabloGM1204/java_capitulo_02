ddfc6cb4ab5162b19208a59f97f401ffd6997b1e
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-11 13:50:52 +0200

    Ejercicio4

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Ejercicio4{
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
+        // Pido que me diga una cantidad y que con la libreria de scanner puede leer por teclado
         System.out.println("Digame una cantidad de euros para pasar a peseta: ");
         double euros = sc.nextDouble();
         double pesetas = 166.386;
